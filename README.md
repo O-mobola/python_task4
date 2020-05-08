edab4b0fd245df7376376607b18df8fd79a539b9
O-mobola <ssalawudeen06@gmail.com>
2020-05-08 23:38:35 +0100

    updated

---
+++ README.md
@@ -0,0 +1,2 @@
+## This program is a simulated banking system
+It run first by prompting for a staff login details username and password. It then check if correspond with registered data. If login is successful, user can then perform 3 operations create new account for customer, check account details, and logout of the ststem
--- task4.py
+++ task4.py
@@ -1,6 +1,7 @@
-
-
 import json
+import os
+from random import*
+
 
 
 #staff login function
@@ -8,24 +9,39 @@ def staff():
 	print("Welcome back please enter your login details")
 	username = input("username: ")
 	password = input("password: ")
+	staff_file = open('staff.txt','r')
+	for list in staff_file:
+		extract = list.split(",")
+		user = extract[0]
+		pswd = extract[1]
+	if username == user and password == pswd:
+		print("successful")
+		staff_session = os.path('session.txt','w+')
+		staff_session.write(username, password)
+		staff_session.close()
 
-
+#staff dashboard if login
 def staffDashboard():
 	print("\nLogin Successful")
-	print("\nenter 'CNA'[Create New Account]")
-	print("\nenter 'CAD'[Check Account Detail]")
-	print("\nenter 'L'[Logout]")
-	prompt2 = input()
+	print("\nenter 'CNA' ->[Create New Account]")
+	print("\nenter 'CAD' ->[Check Account Detail]")
+	print("\nenter 'L' ->[Logout]")
+
+
+#def createAccount():
+	
+	
 	
 
 prompt1 = input("enter 'yes' to login 'no' to exit\n>> ").lower()
-
-if prompt == 'yes':
+if prompt1 == 'yes':
   staff()
-	staff_file = open("staff.txt", "r")
-	staff_file.read()
-if username and password is in staff_file:
-	staffDashboard()
+  staffDashboard()
+
+
+#prompt2 = input("\n>> ").lower()
+#if prompt2 == 'cna':
+	
 		
 else:
 	print("\nHave a nice day!")
