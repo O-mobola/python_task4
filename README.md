97fbc52df0a07102b9a8ed58213a88a529f83ce9
O-mobola <ssalawudeen06@gmail.com>
2020-05-09 22:33:09 +0100

    modified

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-## This program is a simulated banking system
-It run first by prompting for a staff login details username and password. It then check if correspond with registered data. If login is successful, user can then perform 3 operations create new account for customer, check account details, and logout of the ststem
+# This program is a simulated banking system
+It run first by prompting for a staff login details username and password. It then check if correspond with registered data. If login is successful, user can then perform 3 operations create new account for customer, check account details, and logout of the system.
+
+## For testing the default staff login details are:
+(Username : doyin)
+(Password : doyin4#)
+
+
+
--- task4.py
+++ task4.py
@@ -5,68 +5,93 @@ import json
 
 
 staff_session = []
-
+prompt = ""
 #staff login function
 def staff():
 	print("Welcome back please enter your login details")
-	username = input("username: ")
-	password = input("password: ")
-	staff_file = open('staff.txt','r')
-	staff_file.read()
-	for list in staff_file:
-		extract = list.split(",")
-		user = extract[0]
-		pswd = extract[1]
-		if username == user and password == pswd:
-			print("successful")
+	username = str(input("username: "))
+	password = str(input("password: "))
+	staff_file = open("staff.txt","r")
+	staff_file.readline()
+	#staff_file.close()
+	for words in staff_file:
+		extract= words.split(",")
+		user = str(extract[0])
+		pswd = str(extract[1])
+	if username == user and password == pswd:
+			print("Login Successful")
+	else:
+			print("\nIncorrect login details")
+			exit()
 		
 #create session file for successful login
 	session = {
 		"username": username,
 	"password": password
 		}
-	with open("data.txt","w") as file:
+	with open("session.txt","w") as file:
 		json.dump(session, file)
 
 
-#staff dashboard if login
+#function staff dashboard if login
 def staffDashboard():
-	print("\nLogin Successful")
+	print("\nWelcome Back")
 	print("\nenter 'CNA' ->[Create New Account]")
 	print("\nenter 'CAD' ->[Check Account Detail]")
 	print("\nenter 'L' ->[Logout]")
 	
 
+#function create account (cna)
 def createAccount():
-	acct_name = input("\nAccount Name \n> ")
+	acct_name = input("\nAccount Name \n> ").upper()
 	open_bal = int(input("\nOpening Balance \n> "))
-	acct_type = input("\nAccount Type\n> ")
-	acct_email = input("\nAccount Email\n> ")
+	acct_type = input("\nAccount Type\n> ").upper()
+	acct_email = input("\nAccount Email\n> ").lower()
 	for i in range(10):
 		acct_num = randint(0,9)
 		print(acct_num, end="")
 	each_user_data = {
-			"Account_Name": acct_name,
-			"Opening_Balance": open_bal,
-			"Account_Type": acct_type,
-			"Account_Email": acct_email,
-			"Account_Number": acct_num
-		}
-	with open("customer.txt","a") as user:
-		json.dump(each_user_data, user)
+						"Account_Name": acct_name,
+						"Opening_Balance": open_bal,
+						"Account_Type": acct_type,
+						"Account_Email": acct_email,
+						#"Account_Number": acct_num
+					}
+	with open("customer.txt","w") as user:
+			json.dump(each_user_data, user)
 	
+		
+#function check account detail	(cad)
+def checkAccount():
+	acct_num = int(input("\nEnter Account Number\n>> "))
+	with open("customer.txt","r") as fetch:
+		user_data = json.load(fetch)
+		print(user_data)
 	
+#prompt for login
 prompt1 = input("enter 'yes' to login 'no' to exit\n>> ").lower()
 if prompt1 == 'yes':
   staff()
   staffDashboard()
-  
-	
+else:
+	exit()
+  	
 prompt2 = input("\n>> ").lower()
 if prompt2 == 'cna':
 	createAccount()
+	staffDashboarf()
 	
-		
+	#call cad function if prompt is cad	
+elif prompt2 == 'cad':
+	checkAccount()
+	staffDashboard()
+	
+	#call l to logout
+elif prompt2 == 'l':
+		print("\nsee ye around!")
+		os.remove("session.txt")
+		staff()
 else:	
 	print("\nHave a nice day!")
+	os.remove('session.txt')
 	exit()
